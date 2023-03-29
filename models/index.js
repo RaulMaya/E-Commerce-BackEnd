2038bb9c81b5ad0dc302a6f17e0cb64073f2ec9f
Raul <raulmayas20@gmail.com>
2023-03-29 14:27:07 -0600

    Defining models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+})
 
 // Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+})
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  }
+})
+
 
 module.exports = {
   Product,
